1550a53ab8bb763cb1ffaba773abfbb36f5174c9
Peter Mitchell <petemit@gmail.com>
2017-10-30 07:16:17 -0500

    fixed rtl, and made it so apikey is necessary now for cron

--- TimeGoalie/app/src/main/java/us/mindbuilders/petemit/timegoalie/services/TimeGoalieNotifications.java
+++ TimeGoalie/app/src/main/java/us/mindbuilders/petemit/timegoalie/services/TimeGoalieNotifications.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.preference.PreferenceManager;
@@ -23,45 +24,80 @@ import us.mindbuilders.petemit.timegoalie.R;
 
 public class TimeGoalieNotifications {
     private static final int NOTIFICATIONID = 1;
+    private static final int CANCELLATION_TIMEOUT = 2000;
+
+    public enum vibrationPref {
+        vibrate, sound, soundandvibrate
+    }
 
     public static void createNotification(Context context, Intent intent, String injectedMessage) {
-        String message = "";
-        message = intent.getStringExtra(context.getString(R.string.goal_title_key));
-        if (message == null) {
-            message = "Goal finished";
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(context);
+        boolean isSilent = sharedPreferences.getBoolean("pref_silent_mode", false);
+        String selection = sharedPreferences.
+                getString("pref_audio_mode", vibrationPref.soundandvibrate.name());
+        boolean notificationsDisabled = sharedPreferences.
+                getBoolean("pref_disable_app_notifications", false);
+
+        if (!notificationsDisabled) {
+            String message = "";
+            message = intent.getStringExtra(context.getString(R.string.goal_title_key));
+            if (message == null) {
+                message = context.getString(R.string.goal_finished);
+            }
+            NotificationManager notifyMgr = (NotificationManager)
+                    context.getSystemService(Context.NOTIFICATION_SERVICE);
+            Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(context, null)
+                            .setSmallIcon(R.drawable.ic_goalie_with_ball)
+                            .setContentTitle(message)
+                            .setTimeoutAfter(CANCELLATION_TIMEOUT)
+                            .setAutoCancel(true)
+                            .setContentText(injectedMessage);
+
+
+            if (!isSilent) {
+
+                if (selection.equals(vibrationPref.vibrate.name())) {
+                    builder.setDefaults(Notification.DEFAULT_VIBRATE);
+                }
+                if (selection.equals(vibrationPref.sound.name())) {
+                    builder.setSound((soundUri));
+                    builder.setVibrate(new long[]{0L});
+                }
+
+                if (selection.equals(vibrationPref.soundandvibrate.name())) {
+                    builder.setSound((soundUri));
+                    builder.setDefaults(Notification.DEFAULT_VIBRATE);
+                }
+            } else { //might not need anything here
+
+            }
+
+            Intent resultIntent = new Intent(context, GoalListActivity.class);
+
+            PendingIntent pendingIntent =
+                    PendingIntent.getActivity(
+                            context,
+                            0,
+                            resultIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            builder.setContentIntent(pendingIntent);
+
+            notifyMgr.notify(1, builder.build());
         }
-        NotificationManager notifyMgr =(NotificationManager)
-                context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(context,null)
-                .setSmallIcon(R.drawable.ic_goalie_with_ball)
-                .setContentTitle(message)
-                .setAutoCancel(true)
-                .setContentText(injectedMessage)
-                .setSound(soundUri);
-
-        Intent resultIntent = new Intent(context, GoalListActivity.class);
-
-        PendingIntent pendingIntent =
-                PendingIntent.getActivity(
-                        context,
-                        0,
-                        resultIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        builder.setContentIntent(pendingIntent);
-
-        notifyMgr.notify(1,builder.build());
     }
 
     public static void subscribeFromPref(boolean isSubscribed) {
 
         if (isSubscribed) {
             FirebaseMessaging.getInstance().subscribeToTopic("dailyNotification");
-        }
-        else {
+        } else {
             FirebaseMessaging.getInstance().unsubscribeFromTopic("dailyNotification");
         }
     }
--- firebaseServer/functions/index.js
+++ firebaseServer/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const secureCompare = require('secure-compare');
 admin.initializeApp(functions.config().firebase);
 // const express = require('express');
 // const cors = require('cors')({origin: true});
@@ -45,6 +46,19 @@ admin.initializeApp(functions.config().firebase);
 
 
 exports.cronEndpoint = functions.https.onRequest((req, res) => {
+
+const key = req.query.key;
+
+// Exit if the keys don't match
+if (!secureCompare(key, functions.config().cron.key)) {
+  console.log('The key provided in the request does not match the key set in the environment. Check that', key,
+      'matches the cron.key attribute in `firebase env:get`');
+  res.status(403).send('Security key does not match. Make sure your "key" URL query parameter matches the ' +
+      'cron.key environment variable.');
+  return;
+}
+
+
   console.log("woohoo");
 
 const options = {
